74bc38b121dfcbdc59fd578b10da4d8957d72fbe
Mathieu Bocciarelli <mbocciarelli@iutbayonne.univ-pau.fr>
2019-12-08 22:55:09 +0100

    Mise en place du fichier AppFixtures.php et remplissage de la base de donnée

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -6,16 +6,92 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use App\Entity\Formation;
+use App\Entity\Entreprise;
+use App\Entity\Stage;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $formation = new Formation();
-        $formation->setLibelle("DUT INFO");
-        $formation->setNomComplet("Diplome Universitaire Technologique Informatique");
-        $manager->persist($formation);
+      //Création d'un générateur de données Faker
+      $faker = \Faker\Factory::create('fr_FR');
 
-        $manager->flush();
+      //Création des formations
+      $DUT_Info = new Formation();
+      $DUT_Info->setLibelle("DUT INFO");
+      $DUT_Info->setNomComplet("Diplome Universitaire Technologique Informatique");
+      $manager->persist($DUT_Info);
+
+      $LPM = new Formation();
+      $LPM->setLibelle("LPM");
+      $LPM->setNomComplet("License pro multimédia");
+      $manager->persist($LPM);
+
+      $DU_TIC = new Formation();
+      $DU_TIC->setLibelle("DU TIC");
+      $DU_TIC->setNomComplet("Diplome Universitaire Technologique Information et communication");
+      $manager->persist($DU_TIC);
+
+      $tableauFormation = array($DUT_Info, $LPM, $DU_TIC);
+
+      //Création des Entreprises
+      $Capgemini = new Entreprise();
+      $Capgemini->setNom("Capgemini");
+      $Capgemini->setActivite("Informatique");
+      $Capgemini->setAdresse($faker->address);
+      $Capgemini->setSite($faker->url);
+      $manager->persist($Capgemini);
+
+      $Sopra = new Entreprise();
+      $Sopra->setNom("Sopra Steria");
+      $Sopra->setActivite("Informatique");
+      $Sopra->setAdresse($faker->address);
+      $Sopra->setSite($faker->url);
+      $manager->persist($Sopra);
+
+      $Enedis = new Entreprise();
+      $Enedis->setNom("Enedis");
+      $Enedis->setActivite("Energie");
+      $Enedis->setAdresse($faker->address);
+      $Enedis->setSite($faker->url);
+      $manager->persist($Enedis);
+
+      $NRJ = new Entreprise();
+      $NRJ->setNom("NRJ");
+      $NRJ->setActivite("Radio");
+      $NRJ->setAdresse($faker->address);
+      $NRJ->setSite($faker->url);
+      $manager->persist($NRJ);
+
+      $tableauEntreprises = array($Capgemini, $Sopra, $Enedis, $NRJ);
+
+      //Création des offres de Stages
+      $nombreOffresACreer = $faker->numberBetween($min = 5, $max = 15);
+      for ($i=0; $i < $nombreOffresACreer; $i++) {
+          $offre = new Stage();
+          $offre->setTitre($faker->realText($maxNbChars = 45, $indexSize = 2));
+          $offre->setDescription($faker->text($maxNbChars = 255));
+          $offre->setMailContact($faker->safeEmail);
+
+          //Définition de l'entreprise proposant le stage
+          $entrepriseSelectionnee = $faker->numberBetween($min = 0, $max = 3);
+          $offre->setEntreprises($tableauEntreprises[$entrepriseSelectionnee]);
+          $tableauEntreprises[$entrepriseSelectionnee]->addStage($offre);
+
+          //Définition des formations proposé par la stages
+          $formationsNombre = $faker->numberBetween($min = 1, $max = 3);
+          $formationsSelectionnee = $faker->randomElements($array = $tableauFormation, $count = $formationsNombre);
+          foreach ($formationsSelectionnee as $value) {
+            $offre->addFormation($value);
+            $value->addStage($offre);
+          }
+
+          //Ajout aux elements à ajouter à la base de donnée
+          $manager->persist($offre);
+      }
+
+
+      //Transformation en tuplet dans la base de donnée
+      $manager->flush();
     }
 }
